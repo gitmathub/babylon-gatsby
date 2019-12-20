ffaf49f06c10c176c62a7b90fc0e53f84423dd34
gitmatlab <mat@alabaster.de>
2019-12-20 03:58:06 +0100

    hambuger menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 
+// export default () => (
+//   <nav>
+//     <div className="nav_switcher">
+//       <HamburgerMenu />
+//       <MenuBar />
+//     </div>
+//   </nav>
+// )
+
+// export default () => <div className="navigation">HALLO</div>
 export default () => (
-  <nav>
-    <div className="nav">
-      <HamburgerMenu />
-      <MenuBar />
+  <nav className="navigation">
+    <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+    <label for="navi-toggle" className="navigation__button">
+      <span className="navigation__icon">☰</span>
+    </label>
+    <div className="navigation__background">&nbsp;</div>
+    <div className="navigation__nav">
+      <ul className="navigation__list">
+        <li className="navigation__item">
+          <a href="#" className="navigation__link">
+            Kolonialismus
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#" className="navigation__link">
+            Großstädte
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#" className="navigation__link">
+            Kosmopolis
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#" className="navigation__link">
+            Dichtung
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#" className="navigation__link">
+            Blog
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 )
@@ -18,5 +58,3 @@ const MenuBar = () => (
     <span>Blog</span>
   </div>
 )
-
-const HamburgerMenu = () => <button className="nav__hambuger">☰</button>
